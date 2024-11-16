ebb02c74c97342119f5a5bfaa15eb4661dffdf08
Gerardo Belot <gbelot2003@hotmail.com>
2024-11-16 15:41:21 -0600

    :fire:

--- app/Http/Controllers/Web/V1/UserController.php
+++ app/Http/Controllers/Web/V1/UserController.php
@@ -13,4 +13,9 @@ class UserController extends Controller
     {
         return $dataTable->render('users.index');
     }
+
+    public function create()
+    {
+        return view('users.create');
+    }
 }
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte::page')
+
+@section('title', 'Crear Usuario')
+
+@section('content_header')
+    <h1>{{ __('users.create') }}</h1>
+    <div class="card">
+        <div class="card-body">
+
+        </div>
+    </div>
+@stop
